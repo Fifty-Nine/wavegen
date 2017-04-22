3f1c7965bbe8e32a949ddbf6832ad6b958a2a790
Tim Prince <tim.prince@gmail.com>
2017-04-22 13:42:54 -0700

    More windows hacking.

--- software/cmake/FindMPSSE_SPI.cmake
+++ software/cmake/FindMPSSE_SPI.cmake
@@ -1,14 +1,22 @@
 if (LIBMPSSE_SPI_INCLUDE_DIR AND LIBD2XX_LIBRARIES)
   set (LIBMPSSE_SPI_FOUND TRUE)
 else ()
-  find_path(LIBMPSSE_SPI_INCLUDE_DIR MPSSE_SPI.h ftd2xx.h)
+  find_path(LIBMPSSE_SPI_INCLUDE_DIR NAMES MPSSE_SPI.h ftd2xx.h)
   find_library(LIBMPSSE_SPI_LIBRARIES NAMES MPSSE_SPI)
   include(FindPackageHandleStandardArgs)
   find_package_handle_standard_args(
       LIBMPSSE_SPI DEFAULT_MSG LIBMPSSE_SPI_LIBRARIES LIBMPSSE_SPI_INCLUDE_DIR
   )
+endif()
 
-  if (LIBMPSSE_SPI_FOUND AND MSVC_VERSION GREATER 1800)
-    set(CMAKE_REQUIRED_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES} legacy_stdio_definitions.lib)
-  endif()
+# Shoutout to cmake for making my life a living hell.
+if (LIBMPSSE_SPI_FOUND AND MSVC_VERSION GREATER 1800)
+  set(CMAKE_C_STANDARD_LIBRARIES 
+    "${CMAKE_C_STANDARD_LIBRARIES} legacy_stdio_definitions.lib"
+    CACHE STRING "" FORCE
+  )
+  set(CMAKE_CXX_STANDARD_LIBRARIES 
+    "${CMAKE_CXX_STANDARD_LIBRARIES} legacy_stdio_definitions.lib"
+    CACHE STRING "" FORCE
+  )
 endif()
